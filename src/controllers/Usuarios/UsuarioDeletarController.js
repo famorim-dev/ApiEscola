061622632b4110ed13c7e7a101684cef25c8c5a6
Felipe Santos de Amorim <174537470+famorim-dev@users.noreply.github.com>
2025-09-11 15:20:51 -0300

    feature: delete de usuarios criado

---
+++ src/controllers/Usuarios/UsuarioDeletarController.js
@@ -0,0 +1,23 @@
+import Usuarios from "../../models/UsuarioModel"
+
+class UsuarioDeleteController{
+    async delete(req, res) {
+        try{
+            const id = req.params.id
+             if(!id){
+               return res.status(400).json({ erro: 'ID do usuário não fornecido.' })
+            }
+
+            const usuarioDeletado = await Usuarios.destroy({where: {id_usuario: id}})
+            if(usuarioDeletado === 0 ){
+                return res.status(404).json({ erro: 'ID do usuário não encontrado.' })
+            }
+            res.status(200).json({mensagem: 'usuário deletado com sucesso'})
+        }catch(e){
+            console.error('Erro ao deletar usuário:', e);
+            return res.status(500).json({error: 'não foi possivel deletar esse usuário'})
+        }
+    }
+}
+
+export default new UsuarioDeleteController()
--- src/router/UserRoutes.js
+++ src/router/UserRoutes.js
@@ -6,12 +6,13 @@ import UsuarioCadastroController from "../controllers/Usuarios/UsuarioCadastroCo
 import UsuarioBuscarController from "../controllers/Usuarios/UsuarioBuscarController"
 import UsuarioBuscarIdController from "../controllers/Usuarios/UsuarioBuscarIdController"
 import UsuarioEditarController from "../controllers/Usuarios/UsuarioEditarController"
+import UsuarioDeletarController from "../controllers/Usuarios/UsuarioDeletarController"
 
 // Rotas
 userRotas.post('/cadastro', UsuarioCadastroController.create)
 userRotas.get('/buscar', UsuarioBuscarController.index)
 userRotas.get('/buscar/:id', UsuarioBuscarIdController.show)
 userRotas.put('/editar/:id', UsuarioEditarController.update)
-
+userRotas.delete('/deletar/:id', UsuarioDeletarController.delete)
 
 export default userRotas
