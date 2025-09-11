73337882492127e53f9d0d165d8fc7e54f084f96
Felipe Santos de Amorim <174537470+famorim-dev@users.noreply.github.com>
2025-09-11 13:16:11 -0300

    feature: novo controller de buscar usuario pelo id

--- src/router/UserRoutes.js
+++ src/router/UserRoutes.js
@@ -4,9 +4,12 @@ const userRotas = new Router()
 // Import Controllers
 import UsuarioCadastroController from "../controllers/Usuarios/UsuarioCadastroController"
 import UsuarioBuscarController from "../controllers/Usuarios/UsuarioBuscarController"
+import UsuarioBuscarIdController from "../controllers/Usuarios/UsuarioBuscarIdController"
 
 // Rotas
 userRotas.post('/cadastro', UsuarioCadastroController.create)
 userRotas.get('/buscar', UsuarioBuscarController.index)
+userRotas.get('/buscar/:id', UsuarioBuscarIdController.show)
+
 
 export default userRotas
